d270f25ec61363d79dde67e994477bdb6e0884a2
lanbarasan <lanbarasan@ucdavis.edu>
2025-07-09 10:27:26 +0200

    Adding sign in page

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 # app.py
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route('/signin')
+def signin():
+    return render_template('components/signin.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
