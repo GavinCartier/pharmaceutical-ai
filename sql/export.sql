e729d8fe217afffcf1857fba6e5dcd43eee21442
fldoyle <Fdoyle@smu.edu>
2025-07-11 10:34:58 +0100

    Update export.sql

--- sql/export.sql
+++ sql/export.sql
@@ -1,3 +1,10 @@
+/*registration*/ 
+SELECT * FROM users
+INTO OUTFILE '/tmp/pharmacy_users.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
+
 /*New Product*/
 SELECT * FROM productes
 INTO OUTFILE 'productes.csv'
